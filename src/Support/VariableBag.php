15c752fb0af2190cf68cba3738fd60d9ea0ae4cc
Calvin Alkan <calvin@snicco.io>
2021-05-30 23:42:51 -0500

    remove dead code from VariableBag.php

--- src/Support/VariableBag.php
+++ src/Support/VariableBag.php
@@ -10,22 +10,6 @@
 
 	class VariableBag extends Repository {
 
-		/** @var string */
-		private $prefix = 'globals';
-
-		public function setPrefix(string $prefix) : VariableBag {
-
-			$this->prefix = $prefix;
-
-			return $this;
-
-		}
-
-		public function getPrefix() : string {
-
-			return $this->prefix;
-
-		}
 
 		public function add(array $globals) {
 
