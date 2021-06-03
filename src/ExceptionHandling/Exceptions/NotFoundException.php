ebf400c014a0e62f67605a6572beb3ed351bee1f
Calvin Alkan <calvin@snicco.io>
2021-06-03 11:46:26 -0500

    small exception class change

--- src/ExceptionHandling/Exceptions/NotFoundException.php
+++ src/ExceptionHandling/Exceptions/NotFoundException.php
@@ -11,10 +11,10 @@
     class NotFoundException extends HttpException
     {
 
-        public function __construct(int $status_code =404, ?string $message_for_humans = 'We could not find what you are looking for.', Throwable $previous = null, ?int $code = 0)
+        public function __construct(?string $message_for_humans = 'We could not find what you are looking for.', Throwable $previous = null, ?int $code = 0)
         {
 
-            $this->status_code = $status_code;
+            $this->status_code = $message_for_humans;
             $this->message_for_humans = $message_for_humans;
 
         }
