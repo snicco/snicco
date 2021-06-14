93ed71df172c3c7a5d087a95e530c79bccafdb47
Calvin Alkan <calvin@snicco.de>
2021-06-14 02:01:39 -0500

    fixed tests

---
+++ src/EnhancedAuth/EnhancedAuthServiceProvider.php
@@ -0,0 +1,26 @@
+<?php
+
+
+    declare(strict_types = 1);
+
+
+    namespace WPEmerge\EnhancedAuth;
+
+    use WPEmerge\Contracts\ServiceProvider;
+
+    class EnhancedAuthServiceProvider extends ServiceProvider
+    {
+
+        public function register() : void
+        {
+
+
+
+        }
+
+
+        function bootstrap() : void
+        {
+        }
+
+    }
