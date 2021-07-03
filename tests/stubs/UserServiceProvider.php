9530b519a1d832e54d3b2b5857fb6c9ffb485031
Calvin Alkan <calvin@snicco.de>
2021-07-03 14:14:29 -0500

    all tests passing again.

---
+++ tests/stubs/UserServiceProvider.php
@@ -0,0 +1,27 @@
+<?php
+
+
+    declare(strict_types = 1);
+
+
+    namespace Tests\stubs;
+
+    use WPEmerge\Contracts\ServiceProvider;
+
+    class UserServiceProvider extends ServiceProvider
+    {
+
+        public function register() : void
+        {
+
+            $this->container->instance('foo', 'bar');
+
+        }
+
+        public function bootstrap() : void
+        {
+
+            $this->container->instance('foo_bootstrapped', 'bar_bootstrapped');
+        }
+
+    }
