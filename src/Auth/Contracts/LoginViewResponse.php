374f35c6b1dc41ecdcabb9cd2786fc9d3dea0e56
Calvin Alkan <calvin@snicco.de>
2021-08-16 00:04:13 +0200

    add new method withErrors() on the session class.

--- src/Auth/Contracts/LoginViewResponse.php
+++ src/Auth/Contracts/LoginViewResponse.php
@@ -6,22 +6,19 @@
 
     namespace Snicco\Auth\Contracts;
 
-    use Snicco\Auth\Traits\UsesCurrentRequest;
-    use Snicco\Contracts\ResponsableInterface;
-    use Snicco\Http\Psr7\Request;
     use Snicco\Support\Arr;
+    use Snicco\Contracts\ResponsableInterface;
+    use Snicco\Auth\Traits\UsesCurrentRequest;
 
     abstract class LoginViewResponse implements ResponsableInterface
     {
-
-        use UsesCurrentRequest;
-
-        protected Request $request;
-
-        protected array $auth_config;
-
-        public function withAuthConfig(array $config) : LoginViewResponse
-        {
+	
+	    use UsesCurrentRequest;
+	
+	    protected array $auth_config;
+	
+	    public function withAuthConfig(array $config) :LoginViewResponse
+	    {
             $this->auth_config = $config;
             return $this;
         }
