65649421c776f491067043504b2503c55c6f7491
Calvin Alkan <calvin@snicco.de>
2021-08-31 15:00:51 +0200

    remove WithTestTransactions.php trait on DatabaseFactoryTest.php since WpTestCase handles this anyway

--- tests/integration/Database/DatabaseFactoryTest.php
+++ tests/integration/Database/DatabaseFactoryTest.php
@@ -15,7 +15,6 @@ class DatabaseFactoryTest extends DatabaseTestCase
 {
     
     use WithTestTables;
-    use WithTestTransactions;
     
     /** @test */
     public function a_factory_can_be_created()
