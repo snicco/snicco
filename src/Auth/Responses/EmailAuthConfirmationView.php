fee0c35d96468336c45482385e03ece04b58dab3
Calvin Alkan <calvin@snicco.io>
2021-10-11 11:59:00 +0200

    make email auth confirmation view configurable.

---
+++ src/Auth/Responses/EmailAuthConfirmationView.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Snicco\Auth\Responses;
+
+use Snicco\View\ViewFactory;
+use Snicco\Http\Psr7\Request;
+use Snicco\Routing\UrlGenerator;
+use Snicco\Contracts\ViewInterface;
+use Snicco\Auth\Contracts\AbstractEmailAuthConfirmationView;
+
+class EmailAuthConfirmationView extends AbstractEmailAuthConfirmationView
+{
+    
+    private ViewFactory  $view_factory;
+    private UrlGenerator $url;
+    
+    public function __construct(ViewFactory $view_factory, UrlGenerator $url)
+    {
+        $this->view_factory = $view_factory;
+        $this->url = $url;
+    }
+    
+    public function toView(Request $request) :ViewInterface
+    {
+        return $this->view_factory->make('auth-layout')
+                                  ->with([
+                                      'view' => 'auth-confirm-via-email',
+                                      'post_to' => $this->url->toRoute('auth.confirm.email'),
+                                  ]);
+    }
+    
+}
