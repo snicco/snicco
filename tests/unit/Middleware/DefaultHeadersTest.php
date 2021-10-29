746e048b90a2d572cb650f894f24a0c8da067be6
Calvin Alkan <calvin@snicco.io>
2021-10-29 13:27:41 +0200

    adds a new default headers middleware.
    This middleware can be added to every route group in the application config. This is a better approach then adding it the framework config

---
+++ tests/unit/Middleware/DefaultHeadersTest.php
@@ -0,0 +1,60 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\unit\Middleware;
+
+use Tests\UnitTest;
+use Snicco\Http\Delegate;
+use Tests\stubs\TestRequest;
+use Snicco\Middleware\DefaultHeaders;
+use Tests\helpers\CreateUrlGenerator;
+use Tests\helpers\CreatePsr17Factories;
+use Tests\helpers\CreateRouteCollection;
+
+class DefaultHeadersTest extends UnitTest
+{
+    
+    use CreatePsr17Factories;
+    use CreateUrlGenerator;
+    use CreateRouteCollection;
+    
+    /** @test */
+    public function all_headers_are_added_to_the_response()
+    {
+        
+        $next = new Delegate(function ($request) {
+            return $this->createResponseFactory()->make();
+        });
+        
+        $response = (new DefaultHeaders(['foo' => 'bar', 'baz' => 'biz']))->handle(
+            TestRequest::from('GET', 'foo'),
+            $next
+        );
+        
+        $this->assertSame('bar', $response->getHeaderLine('foo'));
+        $this->assertSame('biz', $response->getHeaderLine('baz'));
+        
+    }
+    
+    /** @test */
+    public function x_frame_headers_are_added_by_default()
+    {
+        
+        $next = new Delegate(function ($request) {
+            return $this->createResponseFactory()->make();
+        });
+        
+        $response = (new DefaultHeaders())->handle(
+            TestRequest::from('GET', 'foo'),
+            $next
+        );
+        
+        $this->assertSame(
+            'SAMEORIGIN',
+            $response->getHeaderLine('X-Frame-Options')
+        );
+        
+    }
+    
+}
