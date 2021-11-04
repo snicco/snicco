dd51b2cb18e2c704c3f1058d92838e53a7ba5f33
Calvin Alkan <calvin@snicco.io>
2021-11-04 18:56:42 +0100

    PhpEngine.php: remove dead code

--- src/Contracts/PhpEngine.php
+++ src/Contracts/PhpEngine.php
@@ -7,11 +7,6 @@ namespace Snicco\Contracts;
 interface PhpEngine extends ViewEngineInterface
 {
     
-    /**
-     * Pop the top-most layout content from the stack, render and return it.
-     */
-    public function includeNextView() :void;
-    
     public function renderPhpView(PhpViewInterface $view) :string;
     
 }
