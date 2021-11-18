de78ffd501907a3b6dd6f0eafa8828471178b25d
Calvin Alkan <calvin@snicco.de>
2021-11-18 14:43:01 +0100

    event-dispatcher: add comments to interfaces

--- src/EventDispatcher/GenericEvent.php
+++ src/EventDispatcher/GenericEvent.php
@@ -8,7 +8,14 @@ use Snicco\EventDispatcher\Contracts\Event;
 use Snicco\EventDispatcher\Contracts\CustomizablePayload;
 
 /**
- * @internal
+ * If you dispatch an event as a string instead of using a dedicated class
+ * the event will be transformed into a GenericEvent.
+ * Assuming you would call $dispatcher->dispatch('foo_event', 'bar', ['baz', 'biz]'):
+ * A listener would receive the three arguments in this order. ('bar', ['baz','biz'], 'foo_event').
+ *
+ * @note Events you dispatch as a string ARE NOT MUTABLE and thus can not be filtered.
+ * They can only be used as an action.
+ * @api
  */
 final class GenericEvent implements Event, CustomizablePayload
 {
