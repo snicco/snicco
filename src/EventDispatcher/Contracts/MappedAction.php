31e56fefb421adf965f8390208104977a7e1522f
Calvin Alkan <calvin@snicco.de>
2021-11-18 16:11:19 +0100

    event-dispatcher: document interfaces

--- src/EventDispatcher/Contracts/MappedAction.php
+++ src/EventDispatcher/Contracts/MappedAction.php
@@ -4,6 +4,10 @@ declare(strict_types=1);
 
 namespace Snicco\EventDispatcher\Contracts;
 
+/**
+ * @api
+ * Use this interface if you want to map your event to a WordPress action.
+ */
 interface MappedAction extends Event, IsForbiddenToWordPress
 {
 
