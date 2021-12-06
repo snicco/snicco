07323b86c145c1c66bf597af3f918a660cfb6ef9
Calvin Alkan <calvin@snicco.de>
2021-12-06 09:24:07 +0100

    view,blade: allows referencing views by absolute path.

---
+++ packages/view/tests/integration/PHPViewFactoryTest.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\View\integration;
+
+use Codeception\TestCase\WPTestCase;
+use Snicco\View\ViewComposerCollection;
+use Snicco\View\Implementations\PHPView;
+use Snicco\View\Implementations\PHPViewFinder;
+use Snicco\View\Implementations\PHPViewFactory;
+
+use const DS;
+use const SHARED_FIXTURES_DIR;
+
+final class PHPViewFactoryTest extends WPTestCase
+{
+    
+    /**
+     * @var string
+     */
+    private $view_dir;
+    
+    protected function setUp() :void
+    {
+        parent::setUp();
+        $this->view_dir = SHARED_FIXTURES_DIR.DS.'views';
+    }
+    
+    /** @test */
+    public function a_view_can_be_created_from_an_absolute_path()
+    {
+        $php_view_factory = new PHPViewFactory(
+            new PHPViewFinder([$this->view_dir]),
+            new ViewComposerCollection()
+        );
+        
+        $path = realpath($this->view_dir.'/view.php');
+        
+        $view = $php_view_factory->make([$path]);
+        $this->assertInstanceOf(PHPView::class, $view);
+        $this->assertSame($path, $view->path());
+    }
+    
+}
