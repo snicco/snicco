2b1b04ee6495bec5b7c992b5e442b3101f4bf921
Calvin Alkan <calvin@snicco.de>
2022-01-14 00:18:32 +0100

    core/routing: move RoutingControllers to routing namespace

--- packages/core/src/Routing/Controller/RedirectController.php
+++ packages/core/src/Routing/Controller/RedirectController.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Snicco\Core\Controllers;
+namespace Snicco\Core\Routing\Controller;
 
 use Snicco\Core\Http\Psr7\Request;
 use Snicco\Core\Http\AbstractController;
