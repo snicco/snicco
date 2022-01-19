8f1b726ed0dad70b3d579222b89383c3b1e6b508
Calvin Alkan <calvin@snicco.de>
2022-01-19 01:01:17 +0100

    component/testable-clock: created library

---
+++ src/Snicco/Component/testable-clock/src/Clock.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Snicco\Component\TestableClock;
+
+use DateTimeImmutable;
+
+interface Clock
+{
+    
+    public function currentTimestamp() :int;
+    
+    public function currentTime() :DateTimeImmutable;
+    
+}
