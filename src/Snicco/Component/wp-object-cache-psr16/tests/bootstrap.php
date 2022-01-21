4d65144d56008baf865529bb5419dc2c748df556
Calvin Alkan <calvin@snicco.io>
2022-01-21 14:52:14 +0100

    components/wp-object-cache-psr16: created component.

---
+++ src/Snicco/Component/wp-object-cache-psr16/tests/bootstrap.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+if ( ! file_exists($_SERVER['WP_ROOT_FOLDER'].'/wp-content/object-cache.php')) {
+    echo sprintf(
+        "A persistent wp-object cache drop-in is needed to run the tests for [%s].",
+        dirname(__DIR__)
+    );
+    exit(1);
+}
