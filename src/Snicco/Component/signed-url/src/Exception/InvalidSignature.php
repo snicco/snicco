2ae6a9580757caa76dd3c5228569a862f37d23b0
Calvin Alkan <calvin@snicco.de>
2022-01-30 23:27:09 +0100

    component/signed-url: make all Exceptions extend a base exception class

--- src/Snicco/Component/signed-url/src/Exception/InvalidSignature.php
+++ src/Snicco/Component/signed-url/src/Exception/InvalidSignature.php
@@ -4,12 +4,10 @@ declare(strict_types=1);
 
 namespace Snicco\Component\SignedUrl\Exception;
 
-use RuntimeException;
-
 /**
  * @api
  */
-final class InvalidSignature extends RuntimeException
+final class InvalidSignature extends SignedUrlException
 {
     
 }
--- src/Snicco/Component/signed-url/src/Exception/SignedUrlUsageExceeded.php
+++ src/Snicco/Component/signed-url/src/Exception/SignedUrlUsageExceeded.php
@@ -4,12 +4,10 @@ declare(strict_types=1);
 
 namespace Snicco\Component\SignedUrl\Exception;
 
-use RuntimeException;
-
 /**
  * @api
  */
-final class SignedUrlUsageExceeded extends RuntimeException
+final class SignedUrlUsageExceeded extends SignedUrlException
 {
 
 }
