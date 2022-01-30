cb8a40367f07a920a27be57626cc60500f00b2f8
Calvin Alkan <calvin@snicco.io>
2022-01-30 17:01:16 +0100

    monorepo: create composer test scripts

--- src/Snicco/Component/http-routing/src/AbstractMiddleware.php
+++ src/Snicco/Component/http-routing/src/AbstractMiddleware.php
@@ -48,23 +48,23 @@ abstract class AbstractMiddleware implements MiddlewareInterface
     
     final protected function respond() :ResponseFactory
     {
-        return $this->container[ResponseFactory::class];
+        return $this->container->get(ResponseFactory::class);
     }
     
     final protected function redirect() :Redirector
     {
-        return $this->container[Redirector::class];
+        return $this->container->get(Redirector::class);
     }
     
     final protected function url() :UrlGenerator
     {
-        return $this->container[UrlGenerator::class];
+        return $this->container->get(UrlGenerator::class);
     }
     
     final protected function render(string $template_identifier, array $data = []) :Response
     {
         /** @var TemplateRenderer $renderer */
-        $renderer = $this->container[TemplateRenderer::class];
+        $renderer = $this->container->get(TemplateRenderer::class);
         Assert::isInstanceOf(TemplateRenderer::class, $renderer);
         return $this->respond()->html($renderer->render($template_identifier, $data));
     }
