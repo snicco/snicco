e7190743fefe18a9fe91a963675d7c6aa18a9378
Calvin Alkan <calvin@snicco.io>
2022-01-31 14:45:34 +0100

    root: create tests.yaml workflow

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 use Dotenv\Dotenv;
 
-$dot_env = Dotenv::createImmutable(__DIR__, '.env.testing');
+$dot_env = Dotenv::createImmutable(__DIR__, ['.env.testing', '.env.testing.dist']);
 $dot_env->load();
 $dot_env->required(['WP_ROOT_FOLDER', 'DB_NAME', 'DB_PREFIX', 'DB_USER', 'DB_HOST', 'DB_PASSWORD']);
 
