048471545fe8b3c7c368dc8b91b250cce4ca1e9c
Calvin Alkan <calvin@snicco.io>
2022-02-08 21:41:21 +0100

    bridge/blade: 100% coverage.

--- src/Snicco/Bridge/blade/tests/BladeStandaloneTest.php
+++ src/Snicco/Bridge/blade/tests/BladeStandaloneTest.php
@@ -6,7 +6,13 @@ namespace Snicco\Bridge\Blade\Tests;
 
 use Illuminate\Container\Container;
 use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Events\Dispatcher;
+use Illuminate\Support\Fluent;
+use RuntimeException;
+use Snicco\Bridge\Blade\BladeStandalone;
 use Snicco\Bridge\Blade\DummyApplication;
+use Snicco\Component\Templating\GlobalViewContext;
+use Snicco\Component\Templating\ViewComposer\ViewComposerCollection;
 
 final class BladeStandaloneTest extends BladeTestCase
 {
@@ -30,6 +36,42 @@ final class BladeStandaloneTest extends BladeTestCase
             DummyApplication::class,
             Container::getInstance()->make(Application::class)
         );
+    }
+
+    /**
+     * @test
+     */
+    public function test_exception_if_events_are_dispatched_with_incorrect_payload(): void
+    {
+        $c = Container::getInstance();
+        /** @var Dispatcher $events */
+        $events = $c['events'];
+
+        $this->expectException(RuntimeException::class);
+        $this->expectExceptionMessage('Expected payload[0] to be instance of');
+
+        $events->dispatch('composing: view', ['bogus']);
+    }
+
+    /**
+     * @test
+     */
+    public function test_no_exception_if_config_is_already_instance_of_array_access(): void
+    {
+        Container::setInstance(null);
+
+        $config = new Fluent();
+        $config['foo'] = 'bar';
+        Container::getInstance()['config'] = $config;
+
+        $this->composers = new ViewComposerCollection(
+            null,
+            new GlobalViewContext()
+        );
+        $blade = new BladeStandalone($this->blade_cache, [$this->blade_views], $this->composers);
+        $blade->boostrap();
+
+        $this->assertSame('bar', $config['foo']);
     }
 
 }
