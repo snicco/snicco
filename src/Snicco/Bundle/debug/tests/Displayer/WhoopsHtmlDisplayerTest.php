3978c12b662ce793a0cf9cdb7794f3c0a4a41405
Calvin Alkan <calvin@snicco.de>
2022-02-25 14:56:11 +0100

    bundle/debug: initial implementation

---
+++ src/Snicco/Bundle/debug/tests/Displayer/WhoopsHtmlDisplayerTest.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+
+namespace Snicco\Bundle\Debug\Tests\Displayer;
+
+use PHPUnit\Framework\TestCase;
+use Snicco\Bundle\Debug\Displayer\WhoopsHtmlDisplayer;
+use Whoops\Run;
+
+final class WhoopsHtmlDisplayerTest extends TestCase
+{
+
+    /**
+     * @test
+     */
+    public function test_is_verbose(): void
+    {
+        $displayer = new WhoopsHtmlDisplayer(new Run());
+        $this->assertTrue($displayer->isVerbose());
+    }
+
+
+}
