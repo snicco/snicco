aaafb031dad195e647986ccebe5fc98552b7a069
Calvin Alkan <calvin@snicco.io>
2022-02-26 17:28:31 +0100

    bundle/debug: refactored test cases

---
+++ src/Snicco/Bundle/debug/tests/fixtures/StubDisplayer.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+
+namespace Snicco\Bundle\Debug\Tests\fixtures;
+
+use Snicco\Component\Psr7ErrorHandler\Displayer\ExceptionDisplayer;
+use Snicco\Component\Psr7ErrorHandler\Information\ExceptionInformation;
+
+final class StubDisplayer implements ExceptionDisplayer
+{
+
+    public function display(ExceptionInformation $exception_information): string
+    {
+        return 'stub';
+    }
+
+    public function supportedContentType(): string
+    {
+        return 'text/html';
+    }
+
+    public function isVerbose(): bool
+    {
+        return true;
+    }
+
+    public function canDisplay(ExceptionInformation $exception_information): bool
+    {
+        return true;
+    }
+}
