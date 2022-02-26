7c9802b67bbf3d8a237804307221080553ebd825
Calvin Alkan <calvin@snicco.de>
2022-02-26 22:46:39 +0100

    bundle/http-routing: load configuration from defaults

---
+++ src/Snicco/Bundle/http-routing/config/middleware.php
@@ -0,0 +1,84 @@
+<?php
+
+declare(strict_types=1);
+
+use Snicco\Bundle\HttpRouting\Middleware\ErrorsToExceptions;
+use Snicco\Bundle\HttpRouting\Option\MiddlewareOption;
+use Snicco\Component\HttpRouting\Middleware\RouteRunner;
+use Snicco\Component\HttpRouting\Middleware\RoutingMiddleware;
+use Snicco\Component\HttpRouting\Routing\RoutingConfigurator\RoutingConfigurator;
+
+return [
+
+    /*
+     * Middleware aliases allow you to reference middleware classes by their alias in route definitions.
+     * This can help you to maintain your route files clean and organized. It's however not a requirement
+     * to use middleware aliases.
+     * You can always reference middleware by its full class-name, so feel free to remove this option.
+     */
+    MiddlewareOption::ALIASES => [
+//        'foo' => '\MyPlugin\Middleware\FooMiddleware'
+    ],
+
+    /*
+     * This option defines your custom middleware groups.
+     * The group name is the array key and each group consists of either middleware aliases,class-names or other middleware groups.
+     */
+    MiddlewareOption::GROUPS => [
+        RoutingConfigurator::FRONTEND_MIDDLEWARE => [],
+        RoutingConfigurator::ADMIN_MIDDLEWARE => [],
+        RoutingConfigurator::API_MIDDLEWARE => [],
+        RoutingConfigurator::GLOBAL_MIDDLEWARE => []
+//        'custom_group1' => [
+//            'foo',
+//            'custom_group2'
+//        ],
+//        'custom_group2' => [
+//            'bar'
+//        ]
+    ],
+
+    /*
+     * This option allows you to define an order execution for your middleware
+     * independently of the order that was used when defining the routes.
+     * Middleware that comes first in this array is executed first. Middleware that is not present in this option
+     * will always keep its relative priority.
+     *
+     * Feel free to remove this option if you don't use it in your application.
+     */
+    MiddlewareOption::PRIORITY_LIST => [
+//        '\MyPlugin\Middleware\FooMiddleware',
+//        '\MyPlugin\Middleware\BarMiddleware',
+//        '\MyPlugin\Middleware\BazMiddleware',
+    ],
+
+    /*
+     * By default, if no route matches the current request, no middleware is run.
+     * This option lets you configure this behaviour.
+     * For example: If you put the "admin" group into this array, all middleware from the admin group will
+     * be run for every request to the WordPress admin area including request where none of your admin routes matched.
+     *
+     * Allowed values: "frontend", "api", "global", "admin"
+     */
+    MiddlewareOption::ALWAYS_RUN => [
+//        'frontend',
+//        'api',
+//        'global',
+//        'admin'
+    ],
+
+    /*
+     * This option gives you maximum customizable on how the framework performs its request-response-cycle.
+     * You should leave this value as is unless you really know what you are doing.
+     *
+     * This is different from the "global" middleware group.
+     * Global middleware is attached to all routes after a match was found.
+     * The RoutingMiddleware allows the framework to match a request to a route in the first place.
+     */
+    MiddlewareOption::KERNEL_MIDDLEWARE => [
+        ErrorsToExceptions::class,
+        RoutingMiddleware::class,
+        RouteRunner::class,
+    ]
+
+];
---
+++ src/Snicco/Bundle/http-routing/config/routing.php
@@ -0,0 +1,64 @@
+<?php
+
+declare(strict_types=1);
+
+use Snicco\Bundle\HttpRouting\Option\RoutingOption;
+
+return [
+
+    /*
+     * This configuration value is used for generation URLs among other things.
+     * There are 2 recommended options to set this value depending on your use case:
+     * a) You are distributing code:
+     *    - Parse this value from the site_url WordPress option
+     * b) You are developing for a controlled environment:
+     *    - Load this value from an environment variable using something like symfony/dotenv.
+     */
+    RoutingOption::HOST => '127.0.0.1',
+
+    /*
+     * You can leave this value as is for default WordPress installs.
+     * If you are using Bedrock you would have to set this to /wp/wp-admin.
+     */
+    RoutingOption::WP_ADMIN_PREFIX => '/wp-admin',
+
+    /*
+     * You can leave this value as is for default WordPress installs.
+     * If you have a different (custom) login path you can set it here and the UrlGenerator will
+     * generate correct urls to your custom login endpoint.
+     */
+    RoutingOption::WP_LOGIN_PATH => '/wp-login.php',
+
+    /*
+     * This option should be a list of valid (absolute) directories
+     */
+    RoutingOption::ROUTE_DIRECTORIES => [
+//        dirname(__DIR__) . '/routes'
+    ],
+
+    /*
+     * This option should be a list of valid (absolute) directories.
+     * Feel free to delete it if your project does not need the API routing functionality
+     */
+    RoutingOption::API_ROUTE_DIRECTORIES => [
+//                dirname(__DIR__).'/routes/api',
+//                dirname(__DIR__).'/api-routes'
+    ],
+
+    /*
+     * The API prefix will be used to configure your API routes correctly and to determine if the current request
+     * goes to an API-endpoint.
+     * This option must be set if you are using api-routing. Feel free to remove it otherwise.
+     */
+    RoutingOption::API_PREFIX => '',
+
+    /*
+     * The following three values should only be changed if you are developing on different ports locally.
+     * In that case, you should load these values dynamically from an environment file.
+     */
+    RoutingOption::HTTP_PORT => 80,
+    RoutingOption::HTTPS_PORT => 443,
+    RoutingOption::USE_HTTPS => true
+
+
+];
