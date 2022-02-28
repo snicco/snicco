2d2c0ae93cd87716475b3402c2812069ccd03952
Calvin Alkan <calvin@snicco.de>
2022-02-28 17:45:11 +0100

    component/better-wp-cache: complete refactor + rename of wp-object-cache-psr16
    We use the psr6 cache interface now with an adapter to make it a psr16 cache.
    Furthermore we add support for tagging.

---
+++ src/Snicco/Component/better-wp-cache/src/WPCacheItem.php
@@ -0,0 +1,129 @@
+<?php
+
+declare(strict_types=1);
+
+
+namespace Snicco\Component\BetterWPCache;
+
+use DateInterval;
+use DateTime;
+use DateTimeInterface;
+use Psr\Cache\CacheItemInterface;
+use Snicco\Component\BetterWPCache\Exception\Psr6InvalidArgumentException;
+
+use function gettype;
+use function is_int;
+use function sprintf;
+
+/**
+ * @psalm-internal Snicco\Component\BetterWPCache
+ *
+ * @interal
+ */
+final class WPCacheItem implements CacheItemInterface
+{
+
+    /**
+     * @var mixed
+     */
+    private $value;
+
+    /**
+     * @var non-empty-string
+     */
+    private string $key;
+
+    private bool $is_hit;
+
+    private ?int $expiration_timestamp = null;
+
+    /**
+     * @param non-empty-string $key
+     * @param mixed $value
+     */
+    public function __construct(string $key, $value, bool $is_hit)
+    {
+        $this->key = $key;
+        $this->is_hit = $is_hit;
+        $this->value = $value;
+    }
+
+    /**
+     * @return non-empty-string
+     */
+    public function getKey(): string
+    {
+        return $this->key;
+    }
+
+    public function get()
+    {
+        return $this->value;
+    }
+
+    public function isHit(): bool
+    {
+        return $this->is_hit;
+    }
+
+    public function set($value)
+    {
+        $this->value = $value;
+        $this->is_hit = true;
+        return $this;
+    }
+
+    public function expiresAt($expiration)
+    {
+        if ($expiration instanceof DateTimeInterface) {
+            $this->expiration_timestamp = $expiration->getTimestamp();
+        } elseif (null === $expiration) {
+            $this->expiration_timestamp = $expiration;
+        } else {
+            throw new Psr6InvalidArgumentException(
+                sprintf(
+                    "Cache item ttl/expiresAfter must be \\DateInterval or NULL.\nGot [%s]",
+                    gettype($expiration)
+                )
+            );
+        }
+
+        return $this;
+    }
+
+    public function expiresAfter($time)
+    {
+        if ($time === null) {
+            $this->expiration_timestamp = null;
+        } elseif ($time instanceof DateInterval) {
+            $date = new DateTime();
+            $date->add($time);
+            $this->expiration_timestamp = $date->getTimestamp();
+        } elseif (is_int($time)) {
+            $this->expiration_timestamp = time() + $time;
+        } else {
+            throw new Psr6InvalidArgumentException(
+                sprintf(
+                    "Cache item ttl/expiresAfter must be of type integer or ?\\DateInterval.\nGot [%s]",
+                    gettype($this)
+                )
+            );
+        }
+
+        return $this;
+    }
+
+    public function expirationTimestamp(): ?int
+    {
+        return $this->expiration_timestamp;
+    }
+
+    /**
+     * @param non-empty-string $key
+     */
+    public static function miss(string $key): WPCacheItem
+    {
+        return new self($key, null, false);
+    }
+
+}
---
+++ src/Snicco/Component/better-wp-cache/tests/wordpress/TaggingTest.php
@@ -0,0 +1,315 @@
+<?php
+
+declare(strict_types=1);
+
+
+namespace Snicco\Component\BetterWPCache\Tests\wordpress;
+
+use Cache\TagInterop\TaggableCacheItemPoolInterface;
+use Codeception\TestCase\WPTestCase;
+use RuntimeException;
+use Snicco\Component\BetterWPCache\CacheFactory;
+use stdClass;
+use WP_Object_Cache;
+
+use function method_exists;
+
+final class TaggingTest extends WPTestCase
+{
+
+    /**
+     * @type array with functionName => reason.
+     */
+    protected $skippedTests = [];
+
+    /**
+     * @var TaggableCacheItemPoolInterface
+     */
+    protected $cache;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        global $wp_object_cache;
+
+        if (!$wp_object_cache instanceof WP_Object_Cache) {
+            throw new RuntimeException('wp object cache not setup.');
+        }
+
+        if (!method_exists($wp_object_cache, 'redis_status')) {
+            throw new RuntimeException('wp object cache does not have method redis_status');
+        }
+
+        if (false === $wp_object_cache->redis_status()) {
+            throw new RuntimeException('Redis not running.');
+        }
+    }
+
+    public function createCachePool(): TaggableCacheItemPoolInterface
+    {
+        return CacheFactory::taggable(CacheFactory::psr6('testing'));
+    }
+
+    /**
+     * @before
+     */
+    public function setupService()
+    {
+        $this->cache = $this->createCachePool();
+    }
+
+    /**
+     * @after
+     */
+    public function tearDownService()
+    {
+        if ($this->cache !== null) {
+            $this->cache->clear();
+        }
+    }
+
+    public function invalidKeys()
+    {
+        return [
+            [true],
+            [false],
+            [null],
+            [2],
+            [2.5],
+            ['{str'],
+            ['rand{'],
+            ['rand{str'],
+            ['rand}str'],
+            ['rand(str'],
+            ['rand)str'],
+            ['rand/str'],
+            ['rand\\str'],
+            ['rand@str'],
+            ['rand:str'],
+            [new stdClass()],
+            [['array']],
+        ];
+    }
+
+    public function testMultipleTags()
+    {
+        if (isset($this->skippedTests[__FUNCTION__])) {
+            $this->markTestSkipped($this->skippedTests[__FUNCTION__]);
+        }
+
+        $this->cache->save($this->cache->getItem('key1')->set('value')->setTags(['tag1', 'tag2']));
+        $this->cache->save($this->cache->getItem('key2')->set('value')->setTags(['tag1', 'tag3']));
+        $this->cache->save($this->cache->getItem('key3')->set('value')->setTags(['tag2', 'tag3']));
+        $this->cache->save($this->cache->getItem('key4')->set('value')->setTags(['tag4', 'tag3']));
+
+        $this->cache->invalidateTags(['tag1']);
+        $this->assertFalse($this->cache->hasItem('key1'));
+        $this->assertFalse($this->cache->hasItem('key2'));
+        $this->assertTrue($this->cache->hasItem('key3'));
+        $this->assertTrue($this->cache->hasItem('key4'));
+
+        $this->cache->invalidateTags(['tag2']);
+        $this->assertFalse($this->cache->hasItem('key1'));
+        $this->assertFalse($this->cache->hasItem('key2'));
+        $this->assertFalse($this->cache->hasItem('key3'));
+        $this->assertTrue($this->cache->hasItem('key4'));
+    }
+
+    public function testPreviousTag()
+    {
+        if (isset($this->skippedTests[__FUNCTION__])) {
+            $this->markTestSkipped($this->skippedTests[__FUNCTION__]);
+        }
+
+        $item = $this->cache->getItem('key')->set('value');
+        $tags = $item->getPreviousTags();
+        $this->assertTrue(is_array($tags));
+        $this->assertCount(0, $tags);
+
+        $item->setTags(['tag0']);
+        $this->assertCount(0, $item->getPreviousTags());
+
+        $this->cache->save($item);
+        $this->assertCount(0, $item->getPreviousTags());
+
+        $item = $this->cache->getItem('key');
+        $this->assertCount(1, $item->getPreviousTags());
+    }
+
+    public function testPreviousTagDeferred()
+    {
+        if (isset($this->skippedTests[__FUNCTION__])) {
+            $this->markTestSkipped($this->skippedTests[__FUNCTION__]);
+        }
+
+        $item = $this->cache->getItem('key')->set('value');
+        $item->setTags(['tag0']);
+        $this->assertCount(0, $item->getPreviousTags());
+
+        $this->cache->saveDeferred($item);
+        $this->assertCount(0, $item->getPreviousTags());
+
+        $item = $this->cache->getItem('key');
+        $this->assertCount(1, $item->getPreviousTags());
+    }
+
+    public function testTagAccessorWithEmptyTag()
+    {
+        if (isset($this->skippedTests[__FUNCTION__])) {
+            $this->markTestSkipped($this->skippedTests[__FUNCTION__]);
+        }
+
+        $item = $this->cache->getItem('key')->set('value');
+        $this->expectException('Psr\Cache\InvalidArgumentException');
+        $item->setTags(['']);
+    }
+
+    /**
+     * @dataProvider invalidKeys
+     */
+    public function testTagAccessorWithInvalidTag($tag)
+    {
+        if (isset($this->skippedTests[__FUNCTION__])) {
+            $this->markTestSkipped($this->skippedTests[__FUNCTION__]);
+        }
+
+        $item = $this->cache->getItem('key')->set('value');
+        $this->expectException('Psr\Cache\InvalidArgumentException');
+        $item->setTags([$tag]);
+    }
+
+    public function testTagAccessorDuplicateTags()
+    {
+        if (isset($this->skippedTests[__FUNCTION__])) {
+            $this->markTestSkipped($this->skippedTests[__FUNCTION__]);
+        }
+
+        $item = $this->cache->getItem('key')->set('value');
+        $item->setTags(['tag', 'tag', 'tag']);
+        $this->cache->save($item);
+        $item = $this->cache->getItem('key');
+
+        $this->assertCount(1, $item->getPreviousTags());
+    }
+
+    /**
+     * The tag must be removed whenever we remove an item. If not, when creating a new item
+     * with the same key will get the same tags.
+     */
+    public function testRemoveTagWhenItemIsRemoved()
+    {
+        if (isset($this->skippedTests[__FUNCTION__])) {
+            $this->markTestSkipped($this->skippedTests[__FUNCTION__]);
+        }
+
+        $item = $this->cache->getItem('key')->set('value');
+        $item->setTags(['tag1']);
+
+        // Save the item and then delete it
+        $this->cache->save($item);
+        $this->cache->deleteItem('key');
+
+        // Create a new item (same key) (no tags)
+        $item = $this->cache->getItem('key')->set('value');
+        $this->cache->save($item);
+
+        // Clear the tag, The new item should not be cleared
+        $this->cache->invalidateTags(['tag1']);
+        $this->assertTrue(
+            $this->cache->hasItem('key'),
+            'Item key should be removed from the tag list when the item is removed'
+        );
+    }
+
+    public function testClearPool()
+    {
+        if (isset($this->skippedTests[__FUNCTION__])) {
+            $this->markTestSkipped($this->skippedTests[__FUNCTION__]);
+        }
+
+        $item = $this->cache->getItem('key')->set('value');
+        $item->setTags(['tag1']);
+        $this->cache->save($item);
+
+        // Clear the pool
+        $this->cache->clear();
+
+        // Create a new item (no tags)
+        $item = $this->cache->getItem('key')->set('value');
+        $this->cache->save($item);
+        $this->cache->invalidateTags(['tag1']);
+
+        $this->assertTrue($this->cache->hasItem('key'), 'Tags should be removed when the pool was cleared.');
+    }
+
+    public function testInvalidateTag()
+    {
+        if (isset($this->skippedTests[__FUNCTION__])) {
+            $this->markTestSkipped($this->skippedTests[__FUNCTION__]);
+        }
+
+        $item = $this->cache->getItem('key')->set('value');
+        $item->setTags(['tag1', 'tag2']);
+        $this->cache->save($item);
+        $item = $this->cache->getItem('key2')->set('value');
+        $item->setTags(['tag1']);
+        $this->cache->save($item);
+
+        $this->cache->invalidateTag('tag2');
+        $this->assertFalse($this->cache->hasItem('key'), 'Item should be cleared when tag is invalidated');
+        $this->assertTrue($this->cache->hasItem('key2'), 'Item should be cleared when tag is invalidated');
+
+        // Create a new item (no tags)
+        $item = $this->cache->getItem('key')->set('value');
+        $this->cache->save($item);
+        $this->cache->invalidateTags(['tag2']);
+        $this->assertTrue($this->cache->hasItem('key'), 'Item key list should be removed when clearing the tags');
+
+        $this->cache->invalidateTags(['tag1']);
+        $this->assertTrue($this->cache->hasItem('key'), 'Item key list should be removed when clearing the tags');
+    }
+
+    public function testInvalidateTags()
+    {
+        if (isset($this->skippedTests[__FUNCTION__])) {
+            $this->markTestSkipped($this->skippedTests[__FUNCTION__]);
+        }
+
+        $item = $this->cache->getItem('key')->set('value');
+        $item->setTags(['tag1', 'tag2']);
+        $this->cache->save($item);
+        $item = $this->cache->getItem('key2')->set('value');
+        $item->setTags(['tag1']);
+        $this->cache->save($item);
+
+        $this->cache->invalidateTags(['tag1', 'tag2']);
+        $this->assertFalse($this->cache->hasItem('key'), 'Item should be cleared when tag is invalidated');
+        $this->assertFalse($this->cache->hasItem('key2'), 'Item should be cleared when tag is invalidated');
+
+        // Create a new item (no tags)
+        $item = $this->cache->getItem('key')->set('value');
+        $this->cache->save($item);
+        $this->cache->invalidateTags(['tag1']);
+
+        $this->assertTrue($this->cache->hasItem('key'), 'Item k list should be removed when clearing the tags');
+    }
+
+    /**
+     * When an item is overwritten we need to clear tags for original item.
+     */
+    public function testTagsAreCleanedOnSave()
+    {
+        if (isset($this->skippedTests[__FUNCTION__])) {
+            $this->markTestSkipped($this->skippedTests[__FUNCTION__]);
+        }
+
+        $pool = $this->cache;
+        $i = $pool->getItem('key')->set('value');
+        $pool->save($i->setTags(['foo']));
+        $i = $pool->getItem('key');
+        $pool->save($i->setTags(['bar']));
+        $pool->invalidateTags(['foo']);
+        $this->assertTrue($pool->getItem('key')->isHit());
+    }
+
+}
