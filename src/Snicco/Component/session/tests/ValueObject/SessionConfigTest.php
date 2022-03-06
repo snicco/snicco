d8eb594b7e9e4278ec2fbfa4935b4f0c8ae43a80
Calvin Alkan <calvin@snicco.io>
2022-03-06 19:23:22 +0100

    component/session: remove abs_timeout test

--- src/Snicco/Component/session/tests/ValueObject/SessionConfigTest.php
+++ src/Snicco/Component/session/tests/ValueObject/SessionConfigTest.php
@@ -213,16 +213,5 @@ final class SessionConfigTest extends TestCase
         (new SessionConfig($this->defaults));
     }
 
-    /**
-     * @test
-     */
-    public function test_exception_if_absolute_timeout_negative_int(): void
-    {
-        $this->expectException(InvalidArgumentException::class);
-        $this->expectExceptionMessage('positive-integer');
-
-        $this->defaults['absolute_lifetime_in_sec'] = 0;
-        (new SessionConfig($this->defaults));
-    }
 
 }
