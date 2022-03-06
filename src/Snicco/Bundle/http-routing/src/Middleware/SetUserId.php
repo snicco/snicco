2f436cc34786f2457153805308b4124a5433aacc
Calvin Alkan <calvin@snicco.io>
2022-03-06 18:54:20 +0100

    bundle/http-routing: add SetUserId.php middleware

---
+++ src/Snicco/Bundle/http-routing/src/Middleware/SetUserId.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+
+namespace Snicco\Bundle\HttpRouting\Middleware;
+
+use Psr\Http\Message\ResponseInterface;
+use Snicco\Component\BetterWPAPI\BetterWPAPI;
+use Snicco\Component\HttpRouting\Http\Psr7\Request;
+use Snicco\Component\HttpRouting\Middleware\Middleware;
+use Snicco\Component\HttpRouting\Middleware\NextMiddleware;
+
+final class SetUserId extends Middleware
+{
+
+    private BetterWPAPI $wp;
+
+    public function __construct(BetterWPAPI $wp = null)
+    {
+        $this->wp = $wp ?: new BetterWPAPI();
+    }
+
+    protected function handle(Request $request, NextMiddleware $next): ResponseInterface
+    {
+        $user_id = $this->wp->currentUserId();
+        return $next($request->withUserId($user_id));
+    }
+}
