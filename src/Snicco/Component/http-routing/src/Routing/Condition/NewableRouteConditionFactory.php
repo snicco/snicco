33abfabcfb9e51ebc2efdc440ab85732b9ea1107
Calvin Alkan <calvin@snicco.de>
2022-03-08 19:55:47 +0100

    component/http-routing: make RouteConditionFactory.php an interface.
    The Router class now accepts an optional interface for the RouteConditionFactory.php. The Router now has the correct dependencies instead of relying on a psr container instance.
    
    If resolving conditions via a psr 3 container is desired this can be implemented in user land.

---
+++ src/Snicco/Component/http-routing/src/Routing/Condition/NewableRouteConditionFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+
+namespace Snicco\Component\HttpRouting\Routing\Condition;
+
+use function array_values;
+
+final class NewableRouteConditionFactory implements RouteConditionFactory
+{
+    public function __invoke(ConditionBlueprint $blueprint): RouteCondition
+    {
+        $class = $blueprint->class;
+        $args = $blueprint->passed_args;
+
+        /** @psalm-suppress UnsafeInstantiation */
+        $instance = new $class(...array_values($args));
+
+        if ($blueprint->is_negated) {
+            return new NegatedRouteCondition($instance);
+        }
+        return $instance;
+    }
+}
