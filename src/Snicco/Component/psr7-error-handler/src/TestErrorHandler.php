a3f04d5451fb2117a3d7f9b1584a0d11c7abf402
Calvin Alkan <calvin@snicco.io>
2022-03-08 15:03:41 +0100

    component/psr7-error-handler: added TestErrorHandler.php

---
+++ src/Snicco/Component/psr7-error-handler/src/TestErrorHandler.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+
+namespace Snicco\Component\Psr7ErrorHandler;
+
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Throwable;
+
+final class TestErrorHandler implements HttpErrorHandlerInterface
+{
+
+    public function handle(Throwable $e, ServerRequestInterface $request): ResponseInterface
+    {
+        throw $e;
+    }
+}
