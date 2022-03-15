f2d460ec84ab4027f499a65e4479becf6ed02e63
Calvin Alkan <calvin@snicco.de>
2022-03-15 17:19:46 +0100

    feat: initial commit

---
+++ .releaserc.js
@@ -0,0 +1,35 @@
+module.exports = {
+    "branches": [
+        "master"
+    ],
+    "repositoryUrl": "https://github.com/sniccowp/sniccowp.git",
+    "tagFormat": "v${version}",
+    "plugins": [
+        ["@semantic-release/commit-analyzer", {
+            "preset": "angular",
+            "parserOpts": {
+                "noteKeywords": ["BREAKING CHANGE", "BREAKING CHANGES", "BREAKING"]
+            }
+        }],
+        ["@semantic-release/release-notes-generator", {
+            "preset": "angular",
+            "parserOpts": {
+                "noteKeywords": ["BREAKING CHANGE", "BREAKING CHANGES", "BREAKING"]
+            },
+            "writerOpts": {
+                // Sort by scope first, then by subject.
+                "commitsSort": ["subject", "scope"]
+            }
+        }],
+        "@semantic-release/changelog",
+        "@semantic-release/github",
+        ["@semantic-release/git", {
+            "assets": [
+                "CHANGELOG.md",
+                "src/**/composer.json",
+                "monorepo-builder.php"
+            ],
+            "message": "chore(release): ${nextRelease.version} [skip ci]"
+        }]
+    ]
+}
