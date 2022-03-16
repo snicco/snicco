3eb3e7f32fa687e60de6f392965a8a08b30d618a
Calvin Alkan <calvin@snicco.de>
2022-03-16 21:43:41 +0100

    chore(monorepo): allow * as a commit scope

--- bin/php/generate-commit-scopes.php
+++ bin/php/generate-commit-scopes.php
@@ -4,12 +4,12 @@ declare(strict_types=1);
 
 use Snicco\Component\StrArr\Str;
 use Snicco\Monorepo\SniccoWPPackageProvider;
+use Webmozart\Assert\Assert;
 
 require_once dirname(__DIR__, 2) . '/vendor/autoload.php';
 
-
 try {
-    $extra_scopes = ['monorepo',];
+    $extra_scopes = ['monorepo', '*'];
     $components = [];
     $middleware = [];
     $bundles = [];
@@ -31,16 +31,17 @@ try {
             $components[] = $name;
         }
     }
+
     sort($components);
     sort($bridge);
     sort($bundles);
     sort($middleware);
 
-    $merged = array_merge($extra_scopes, $components, $bridge, $bundles, $middleware);
+    $merged = [...$extra_scopes, ...$components, ...$bridge, ...$bundles, ...$middleware];
 
     $json = json_encode($merged, JSON_THROW_ON_ERROR | JSON_PRETTY_PRINT);
     $res = file_put_contents($f = dirname(__DIR__, 2) . '/commit-scopes.json', (string)$json);
-    Webmozart\Assert\Assert::notFalse($res, 'Could not update commit scopes.');
+    Assert::notFalse($res, 'Could not update commit scopes.');
     echo sprintf("Updated commit scopes at [%s]\n", $f);
 } catch (Throwable $e) {
     echo $e->getMessage();
