785822fb238759cf0c32b597fc085c0900380a32
Calvin Alkan <calvin@snicco.de>
2022-03-16 19:02:36 +0100

    ci(monorepo): dont include experimental packages in split-matrix

--- bin/php/monorepo_split_matrix.php
+++ bin/php/monorepo_split_matrix.php
@@ -2,6 +2,8 @@
 
 declare(strict_types=1);
 
+use Snicco\Component\StrArr\Str;
+use Snicco\Monorepo\Package\Package;
 use Snicco\Monorepo\SniccoWPPackageProvider;
 
 require_once dirname(__DIR__, 2) . '/vendor/autoload.php';
@@ -9,7 +11,13 @@ require_once dirname(__DIR__, 2) . '/vendor/autoload.php';
 try {
     $package_provider = SniccoWPPackageProvider::create();
 
-    $packages = json_encode($package_provider->getAll(), JSON_THROW_ON_ERROR);
+    $packages = $package_provider->getAll();
+
+    $packages = $packages->filter(function (Package $package): bool {
+        return !Str::contains($package->name, 'eloquent');
+    });
+
+    $packages = json_encode($packages, JSON_THROW_ON_ERROR);
 } catch (Throwable $e) {
     echo $e->getMessage();
     exit(1);
