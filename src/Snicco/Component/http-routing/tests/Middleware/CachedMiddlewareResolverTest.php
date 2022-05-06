577e5b1d2de468b1348c3db45d48b134545695f2
Calvin Alkan <calvin@snicco.io>
2022-05-06 13:52:42 +0200

    chore(monorepo): remove rector from lint commands
    See: 69d5d58017de7abd96054024e561c9431a9a6e88

--- src/Snicco/Component/http-routing/tests/Middleware/CachedMiddlewareResolverTest.php
+++ src/Snicco/Component/http-routing/tests/Middleware/CachedMiddlewareResolverTest.php
@@ -24,103 +24,102 @@ use Snicco\Component\HttpRouting\Tests\HttpRunnerTestCase;
  */
 final class CachedMiddlewareResolverTest extends HttpRunnerTestCase
 {
-    
     /**
      * @test
      */
-    public function the_middleware_resolver_can_be_constructed_from_cache() :void
+    public function the_middleware_resolver_can_be_constructed_from_cache(): void
     {
         $blueprint1 = MiddlewareBlueprint::from(FooMiddleware::class, ['bar', 'baz']);
         $blueprint2 = MiddlewareBlueprint::from(BarMiddleware::class, ['biz']);
-        
+
         $route_cache = [
             'r1' => [$blueprint1->asArray(), $blueprint2->asArray()],
         ];
-        
+
         $resolver = MiddlewareResolver::fromCache($route_cache, [
             RoutingConfigurator::GLOBAL_MIDDLEWARE => [$blueprint1->asArray()],
             RoutingConfigurator::FRONTEND_MIDDLEWARE => [$blueprint2->asArray()],
         ]);
-        
+
         $route = Route::create('/', Route::DELEGATE, 'r1');
         $route->middleware(BazMiddleware::class);
-        
+
         $controller_action = new ControllerAction(Route::DELEGATE, $this->psr_container);
-        
+
         // BazMiddleware is not present since It's loaded from cache.
         $this->assertEquals([$blueprint1, $blueprint2], $resolver->resolveForRoute($route, $controller_action));
-        
+
         $this->assertEquals(
             [$blueprint1, $blueprint2],
             $resolver->resolveForRequestWithoutRoute($this->frontendRequest())
         );
-        
+
         $this->assertEquals([$blueprint1], $resolver->resolveForRequestWithoutRoute($this->adminRequest('/foo')));
-        
+
         $this->assertEquals([$blueprint1], $resolver->resolveForRequestWithoutRoute($this->adminRequest('/foo')));
     }
-    
+
     /**
      * @test
      */
-    public function middleware_for_api_requests_can_be_loaded_from_cache() :void
+    public function middleware_for_api_requests_can_be_loaded_from_cache(): void
     {
         $blueprint1 = MiddlewareBlueprint::from(FooMiddleware::class, ['bar', 'baz']);
         $blueprint2 = MiddlewareBlueprint::from(BarMiddleware::class, ['biz']);
-        
+
         $resolver = MiddlewareResolver::fromCache([], [
             RoutingConfigurator::GLOBAL_MIDDLEWARE => [$blueprint1->asArray()],
             RoutingConfigurator::API_MIDDLEWARE => [$blueprint2->asArray()],
         ]);
-        
+
         $this->assertEquals(
             [$blueprint1, $blueprint2],
             $resolver->resolveForRequestWithoutRoute($this->apiRequest())
         );
-        
+
         $this->assertEquals([$blueprint1], $resolver->resolveForRequestWithoutRoute($this->adminRequest('/foo')));
-        
+
         $this->assertEquals(
             [$blueprint1],
             $resolver->resolveForRequestWithoutRoute($this->frontendRequest('/foo'))
         );
     }
-    
+
     /**
      * @test
      */
-    public function test_exception_if_a_route_is_not_in_cache() :void
+    public function test_exception_if_a_route_is_not_in_cache(): void
     {
         $blueprint1 = MiddlewareBlueprint::from(FooMiddleware::class, ['bar', 'baz']);
         $blueprint2 = MiddlewareBlueprint::from(BarMiddleware::class, ['biz']);
-        
+
         $route_cache = [
             'r2' => [$blueprint1->asArray(), $blueprint2->asArray()],
         ];
-        
+
         $resolver = MiddlewareResolver::fromCache($route_cache, [
             RoutingConfigurator::GLOBAL_MIDDLEWARE => [$blueprint1->asArray()],
             RoutingConfigurator::FRONTEND_MIDDLEWARE => [$blueprint2->asArray()],
         ]);
-        
+
         $route = Route::create('/', Route::DELEGATE, 'r1');
-        
+
         $this->expectException(LogicException::class);
         $this->expectExceptionMessage('The middleware resolver is cached but has no entry for route [r1].');
         $resolver->resolveForRoute($route, new ControllerAction(Route::DELEGATE, $this->psr_container));
     }
-    
+
     /**
      * @test
      */
-    public function test_create_middleware_cache() :void
+    public function test_create_middleware_cache(): void
     {
         $route1 = Route::create('/foo', Route::DELEGATE, 'r1')->middleware('group1');
         $route2 = Route::create('/bar', Route::DELEGATE, 'r2')->middleware('foo:FOO');
         $route3 = Route::create('/baz', ControllerWithBarMiddleware::class, 'r3')->middleware(['foo:FOO']);
-        
+
         $routes = new RouteCollection([$route1, $route2, $route3]);
-        
+
         $resolver = new MiddlewareResolver(
             [RoutingConfigurator::FRONTEND_MIDDLEWARE],
             [
@@ -134,24 +133,24 @@ final class CachedMiddlewareResolverTest extends HttpRunnerTestCase
             ],
             [BarMiddleware::class]
         );
-        
+
         $pimple = new Container();
         $psr = new \Pimple\Psr11\Container($pimple);
-        
+
         $cache = $resolver->createMiddlewareCache($routes, $psr);
-        
+
         $this->assertTrue(isset($cache['route_map']));
         $this->assertTrue(isset($cache['route_map']));
         $this->assertTrue(isset($cache['route_map']['r1']));
         $this->assertTrue(isset($cache['route_map']['r2']));
         $this->assertTrue(isset($cache['route_map']['r3']));
-        
+
         $this->assertTrue(isset($cache['request_map']));
         $this->assertTrue(isset($cache['request_map']['frontend']));
         $this->assertTrue(isset($cache['request_map']['api']));
         $this->assertTrue(isset($cache['request_map']['admin']));
         $this->assertTrue(isset($cache['request_map']['global']));
-        
+
         $this->assertSame([
             [
                 'class' => BarMiddleware::class,
@@ -162,14 +161,14 @@ final class CachedMiddlewareResolverTest extends HttpRunnerTestCase
                 'args' => [],
             ],
         ], $cache['route_map']['r1']);
-        
+
         $this->assertSame([
             [
                 'class' => FooMiddleware::class,
                 'args' => ['FOO'],
             ],
         ], $cache['route_map']['r2']);
-        
+
         $this->assertSame([
             [
                 'class' => BarMiddleware::class,
@@ -180,11 +179,11 @@ final class CachedMiddlewareResolverTest extends HttpRunnerTestCase
                 'args' => ['FOO'],
             ],
         ], $cache['route_map']['r3']);
-        
+
         $this->assertSame([], $cache['request_map']['global']);
         $this->assertSame([], $cache['request_map']['api']);
         $this->assertSame([], $cache['request_map']['admin']);
-        
+
         // Bar has higher priority
         $this->assertSame([
             [
@@ -197,11 +196,11 @@ final class CachedMiddlewareResolverTest extends HttpRunnerTestCase
             ],
         ], $cache['request_map']['frontend']);
     }
-    
+
     /**
      * @test
      */
-    public function creating_the_middleware_cache_takes_settings_for_always_run_into_account() :void
+    public function creating_the_middleware_cache_takes_settings_for_always_run_into_account(): void
     {
         $resolver = new MiddlewareResolver(
             [RoutingConfigurator::FRONTEND_MIDDLEWARE],
@@ -218,48 +217,48 @@ final class CachedMiddlewareResolverTest extends HttpRunnerTestCase
             ],
             [BarMiddleware::class]
         );
-        
+
         $pimple = new Container();
         $psr = new \Pimple\Psr11\Container($pimple);
-        
+
         $cache = $resolver->createMiddlewareCache(new RouteCollection(), $psr);
-        
+
         $request_map = $cache['request_map'];
-        
+
         $this->assertTrue(isset($request_map['frontend']), 'Frontend middleware should be in request_map cache');
         $this->assertTrue(isset($request_map['api']), 'API middleware should have been in request_map cache');
         $this->assertTrue(isset($request_map['admin']), 'Admin middleware should have been in request_map cache');
         $this->assertTrue(isset($request_map['global']), 'Global middleware should have been in request_map cache');
-        
+
         $this->assertSame([
             [
                 'class' => FooMiddleware::class,
-                'args' => []
+                'args' => [],
             ],
         ], $request_map['frontend']);
-    
+
         $this->assertSame([], $request_map['api']);
         $this->assertSame([], $request_map['admin']);
         $this->assertSame([], $request_map['global']);
-        
+
         $resolver = MiddlewareResolver::fromCache($cache['route_map'], $cache['request_map']);
-    
+
         $this->assertEquals([
-            new MiddlewareBlueprint(FooMiddleware::class, [])
+            new MiddlewareBlueprint(FooMiddleware::class, []),
         ], $resolver->resolveForRequestWithoutRoute($this->frontendRequest()));
-        
+
         $this->assertEquals([], $resolver->resolveForRequestWithoutRoute($this->adminRequest('/wp-admin')));
         $this->assertEquals([], $resolver->resolveForRequestWithoutRoute($this->apiRequest()));
     }
-    
+
     /**
      * @test
      */
-    public function middleware_caching_detects_recursion() :void
+    public function middleware_caching_detects_recursion(): void
     {
         $this->expectException(MiddlewareRecursion::class);
         $this->expectExceptionMessage('Detected middleware recursion: global->group2->group3->group2');
-        
+
         new MiddlewareResolver(
             [],
             [],
@@ -273,5 +272,4 @@ final class CachedMiddlewareResolverTest extends HttpRunnerTestCase
             []
         );
     }
-    
 }
