6411cd5c1e9854ed17dae625d01bc32cd7e42084
Calvin Alkan <calvin@snicco.io>
2023-08-13 16:09:57 +0200

    feat(http-routing,http-routing-bundle): allow url generation context from runtime values

--- src/Snicco/Bundle/http-routing/src/HttpRoutingBundle.php
+++ src/Snicco/Bundle/http-routing/src/HttpRoutingBundle.php
@@ -76,6 +76,7 @@ use function copy;
 use function count;
 use function dirname;
 use function gettype;
+use function home_url;
 use function implode;
 use function in_array;
 use function interface_exists;
@@ -152,12 +153,17 @@ final class HttpRoutingBundle implements Bundle
 
         $kernel->afterConfiguration(function (WritableConfig $config) use ($kernel_dirs) {
             // quick type checks.
-            $config->getInteger('routing.' . RoutingOption::HTTP_PORT);
-            $config->getInteger('routing.' . RoutingOption::HTTPS_PORT);
-            $config->getBoolean('routing.' . RoutingOption::USE_HTTPS);
-
-            Assert::stringNotEmpty(
-                $config->getString('routing.' . RoutingOption::HOST),
+            Assert::nullOrPositiveInteger(
+                $config->getIntegerOrNull('routing.' . RoutingOption::HTTP_PORT)
+            );
+            Assert::nullOrPositiveInteger(
+                $config->getIntegerOrNull('routing.' . RoutingOption::HTTPS_PORT)
+            );
+            Assert::nullOrBoolean(
+                $config->getBooleanOrNull('routing.' . RoutingOption::USE_HTTPS)
+            );
+            Assert::nullOrstringNotEmpty(
+                $config->getStringOrNull('routing.' . RoutingOption::HOST),
                 'routing.' . RoutingOption::HOST . ' must be a non-empty-string.'
             );
 
@@ -439,18 +445,33 @@ final class HttpRoutingBundle implements Bundle
                 $config = $kernel->config();
                 $env = $kernel->env();
 
-                $context = new UrlGenerationContext(
-                    $config->getString('routing.' . RoutingOption::HOST),
-                    $config->getInteger('routing.' . RoutingOption::HTTPS_PORT),
-                    $config->getInteger('routing.' . RoutingOption::HTTP_PORT),
-                    $config->getBoolean('routing.' . RoutingOption::USE_HTTPS)
-                );
+                $host = $config->getStringOrNull('routing.' . RoutingOption::HOST);
+                $https_port = $config->getIntegerOrNull('routing.' . RoutingOption::HTTPS_PORT);
+                $http_port = $config->getIntegerOrNull('routing.' . RoutingOption::HTTP_PORT);
+                $https_by_default = $config->getBooleanOrNull('routing.' . RoutingOption::USE_HTTPS);
+
+                if (isset($host, $https_port, $http_port, $https_by_default)) {
+                    $context = new UrlGenerationContext(
+                        $host,
+                        $https_port,
+                        $http_port,
+                        $https_by_default
+                    );
+                } else {
+                    $context = UrlGenerationContext::fromUrlAndParts(
+                        home_url(),
+                        $host,
+                        $https_port,
+                        $http_port,
+                        $https_by_default,
+                    );
+                }
 
                 $loader = $container[RouteLoader::class] ?? $container[PHPFileRouteLoader::class];
 
                 $cache = ($env->isStaging() || $env->isProduction())
-                ? new FileRouteCache($kernel->directories()->cacheDir() . '/prod.routes-generated.php')
-                : new NullCache();
+                    ? new FileRouteCache($kernel->directories()->cacheDir() . '/prod.routes-generated.php')
+                    : new NullCache();
 
                 $admin_area = new WPAdminArea(
                     $config->getString('routing.' . RoutingOption::WP_ADMIN_PREFIX),
