cdf93358b85640b270287262e8914fb3e451e731
Calvin Alkan <calvin@snicco.io>
2024-04-26 23:40:02 +0200

    fix(better-wpdb): make read locks compatible with mariadb
    Fix: #192

--- bin/up.sh
+++ bin/up.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 
-CONTAINER_NAME="mysql-snicco"
-MYSQL_PORT=${1:-3306}
+IMAGE=${1:-"mysql:8.0.28"}
+#(mariadb:latest-snicco), only [a-zA-Z0-9][a-zA-Z0-9_.-]
+CONTAINER_NAME=$(echo "$IMAGE-snicco" | tr ":" "-")
+MYSQL_PORT=${2:-3306}
 
 # Check if the container exists
-if [ "$(docker ps -aq -f name=$CONTAINER_NAME)" ]; then
+if [ "$(docker ps -aq -f name="$CONTAINER_NAME")" ]; then
   # Stop the container
-  docker stop $CONTAINER_NAME
-  docker rm $CONTAINER_NAME
+  docker stop "$CONTAINER_NAME"
+  docker rm "$CONTAINER_NAME"
 fi
 
-docker run -p "$MYSQL_PORT":3306 --name "$CONTAINER_NAME" -e MYSQL_ROOT_PASSWORD=root -e MYSQL_DATABASE=snicco_1 -d mysql:8.0.28
+docker run -p "$MYSQL_PORT":3306 --name "$CONTAINER_NAME" -e MYSQL_ROOT_PASSWORD=root -e MYSQL_DATABASE=snicco_1 -d "$IMAGE"
